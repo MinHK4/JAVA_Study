15b525aad5471adde0b6a97b4e6376744d51e308
MinHK4 <dzf1224@naver.com>
2023-06-08 10:25:26 +0900

    Code20: Contact Bubble Sort

--- JAVA_DATA_STRUCTURE/Chapter01/Section2/Code20.java
+++ JAVA_DATA_STRUCTURE/Chapter01/Section2/Code20.java
@@ -2,14 +2,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Code19 {
+public class Code20 {
 
+	static String [] name = new String[1000];
+	static String [] phoneNumber = new String[1000];
+	static int n = 0;
+	
 	public static void main(String[] args) {
-		
-		String [] name = new String[1000];
-		String [] phoneNumber = new String[1000];
-		int n = 0;
-		
 		
 		// Java 프로젝트 최상단을 기본 위치로 해서 파일 시스템 읽어옴
 		// 해당 파일명이 존재하지 않을 수도 있는 경우에 대한 예외처리
@@ -31,9 +30,27 @@ public class Code19 {
 			System.exit(1);
 		}
 		
+		// 정의한 메소드 활용해서 연락처 이름 순으로 정렬
+		bubbleSortByName();
 		
 		for(int i=0; i<n; i++)
 			System.out.println(name[i] + " : " + phoneNumber[i]);
 	}
-
+	
+	static void bubbleSortByName() {
+		for(int i=n-1; i>0; i--) {
+			for(int j=0; j<i; j++) {
+				if(name[j].compareTo(name[j+1]) > 0) {
+					String temp = name[j];
+					name[j] = name[j+1];
+					name[j+1] = temp;
+					
+					temp = phoneNumber[j];
+					phoneNumber[j] = phoneNumber[j+1];
+					phoneNumber[j+1] = temp;
+				}
+			}
+		}
+	}
+	
 }
